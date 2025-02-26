a020bbffe6e6d78ec234dc7fb57487851c5ea195
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2025-02-26 10:00:59 -0700

    initial commit with PSM unit testing structure

---
+++ j2735-2024/src/test/java/j2735/PersonalSafetyMessage/PersonalSafetyMessageTest.java
@@ -0,0 +1,42 @@
+package j2735.PersonalSafetyMessage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+import j2735.BaseSerializeTest;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+public class PersonalSafetyMessageTest extends BaseSerializeTest<PersonalSafetyMessage> {
+
+  public PersonalSafetyMessageTest() {
+    super(PersonalSafetyMessage.class);
+  }
+
+  @Test
+  public void xmlDeserialize_generatedXml() throws IOException {
+    PersonalSafetyMessage psm = fromXml(loadResource("/j2735/PersonalSafetyMessage/xml/GeneratedPsm.xml"));
+    assertThat(psm, notNullValue());
+
+    String json = toJson(psm);
+    System.out.println(json);
+  }
+
+  @Test
+  public void xmlDeserialize_generatedXml_2() throws IOException {
+    PersonalSafetyMessage psm = fromXml(loadResource("/j2735/PersonalSafetyMessage/xml/GeneratedPsm2.xml"));
+    assertThat(psm, notNullValue());
+
+    String json = toJson(psm);
+    System.out.println(json);
+  }
+
+  @Test
+  public void xmlDeserialize_generatedXml_erlang() throws IOException {
+    PersonalSafetyMessage psm = fromXml(loadResource("/j2735/PersonalSafetyMessage/xml/GeneratedPsmErlang.xml"));
+    assertThat(psm, notNullValue());
+
+    String json = toJson(psm);
+    System.out.println(json);
+  }
+}
