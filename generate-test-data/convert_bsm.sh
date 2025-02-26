2eb9dbe8c10821264056a77f61ed1bd4802da25d
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-02-26 14:22:31 -0700

    Fix converter-all with VehicleEventFlags update.  Generate BSMs with VehicleSafetyExtensions.

--- generate-test-data/convert_bsm.sh
+++ generate-test-data/convert_bsm.sh
@@ -2,5 +2,6 @@
 
 set -x
 
-./converter-all -dd -p BasicSafetyMessage -iuper -oxer $1.bin > $1.xml
-./converter-all -dd -p BasicSafetyMessage -iuper -ojer $1.bin > $1.json
+BASEDIR=./examples/2024/bsm
+./converter-all -dd -p BasicSafetyMessage -iuper -oxer $BASEDIR/$1.bin > $BASEDIR/$1.xml
+./converter-all -dd -p BasicSafetyMessage -iuper -ojer $BASEDIR/$1.bin > $BASEDIR/$1.json
