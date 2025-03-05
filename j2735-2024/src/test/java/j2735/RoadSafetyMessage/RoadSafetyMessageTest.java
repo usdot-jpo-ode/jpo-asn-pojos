c14ac957e836a88c0cb1e60a8579e1f7a007e58a
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2025-03-05 12:30:06 -0700

    initial RSM testing

---
+++ j2735-2024/src/test/java/j2735/RoadSafetyMessage/RoadSafetyMessageTest.java
@@ -0,0 +1,60 @@
+package j2735.RoadSafetyMessage;
+
+import j2735.BaseSerializeTest;
+import j2735.RoadSafetyMessage.RoadSafetyMessage;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static net.javacrumbs.jsonunit.JsonMatchers.jsonEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.xmlunit.matchers.CompareMatcher.isIdenticalTo;
+
+public class RoadSafetyMessageTest extends BaseSerializeTest<RoadSafetyMessage> {
+
+  public RoadSafetyMessageTest() {
+    super(RoadSafetyMessage.class);
+  }
+
+  @ParameterizedTest
+  @MethodSource("getXmlResourcesRoot")
+  public void canRoundTripXml(String resourcePath) throws IOException {
+    String xml = loadResource(resourcePath);
+    RoadSafetyMessage rsm = fromXml(xml);
+    assertThat(rsm, notNullValue());
+    String roundTripXml = toXml(rsm);
+    assertThat(roundTripXml, isIdenticalTo(xml).ignoreComments().ignoreWhitespace());
+  }
+
+//  @ParameterizedTest
+//  @MethodSource("getJsonResources")
+//  public void canRoundTripJson_PropulsionTypesTest(String resourcePath) throws IOException {
+//    String json = loadResource(resourcePath);
+//    RoadSafetyMessage rsm = fromJson(json);
+//    assertThat(rsm, notNullValue());
+//    String roundTripJson = toJson(rsm);
+//    assertThat(roundTripJson, jsonEquals(json));
+//  }
+
+  private static Stream<Arguments> getXmlResourcesRoot() {
+    return getXmlResources("");
+  }
+
+  private static Stream<Arguments> getJsonResourcesPropulsionTypes() {
+    return getJsonResources("propulsion_types");
+  }
+
+  private static Stream<Arguments> getXmlResources(String subdirectory)  {
+    return getResources("/j2735/RoadSafetyMessage/xml/" + subdirectory);
+  }
+
+  private static Stream<Arguments> getJsonResources(String subdirectory) {
+    return getResources("/j2735/RoadSafetyMessage/json/" + subdirectory);
+  }
+
+}
