122f142bb28210bfd32aff5ad0be017ae21eda48
Drew Johnston <31270488+drewjj@users.noreply.github.com>
2025-03-06 03:56:12 -0700

    Add SDSM example messages and tests for full code coverage of SDSM POJOs

---
+++ j2735-2024/src/test/java/j2735/SensorDataSharingMessage/RollDetectedTest.java
@@ -0,0 +1,20 @@
+package j2735.SensorDataSharingMessage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the RollDetected class.
+ */
+public class RollDetectedTest {
+  
+  @Test
+  public void constructorTest() {
+    var rollDetected = new RollDetected(10L);
+    assertThat(rollDetected, notNullValue());
+    assertThat(rollDetected.getValue(), equalTo(10L));
+  }
+}
---
+++ j2735-2024/src/test/java/j2735/SensorDataSharingMessage/YawDetectedTest.java
@@ -0,0 +1,20 @@
+package j2735.SensorDataSharingMessage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the YawDetected class.
+ */
+public class YawDetectedTest {
+  
+  @Test
+  public void constructorTest() {
+    var yawDetected = new YawDetected(25L);
+    assertThat(yawDetected, notNullValue());
+    assertThat(yawDetected.getValue(), equalTo(25L));
+  }
+}
