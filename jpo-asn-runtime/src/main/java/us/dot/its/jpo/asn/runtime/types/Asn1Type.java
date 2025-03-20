aeacbf75a126ea1d88e80579ef7b347026ade23e
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-03-20 15:13:26 -0600

    More detailed javadoc on Asn1Type

--- jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/types/Asn1Type.java
+++ jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/types/Asn1Type.java
@@ -2,7 +2,9 @@ package us.dot.its.jpo.asn.runtime.types;
 
 
 /**
- * Interface implemented by all ASN.1 type classes.
+ * This is a marker interface.  It is the root of the hierarchy of classes representing
+ * ASN.1 types in this package.  Clients can use it to represent any ASN.1 type whose specific
+ * subtype is unknown.
  */
 public interface Asn1Type {
 
--- jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/types/UnknownType.java
+++ jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/types/UnknownType.java
@@ -5,7 +5,7 @@ package us.dot.its.jpo.asn.runtime.types;
  * This class is emitted by the compiler if it doesn't recognize a syntactic element in an ASN.1
  * type assignment.
  * If classes of this type exist, it means the ASN.1 specification has unsupported features.
- * If no classes of this type exists, it means all type assignments were recognized by the compiler.
+ * If no classes of this type exist, it means all type assignments were recognized by the compiler.
  * @author Ivan Yourshaw
   */
 public class UnknownType implements Asn1Type {
