1a35d215307451bd2c37af876cb505cdf8820ae6
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-03-20 15:05:13 -0600

    More detailed doc for UnknownType

--- jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/types/UnknownType.java
+++ jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/types/UnknownType.java
@@ -2,7 +2,10 @@ package us.dot.its.jpo.asn.runtime.types;
 
 /**
  * Placeholder for unknown types in the generated source.
+ * This class is emitted by the compiler if it doesn't recognize a syntactic element in an ASN.1
+ * type assignment.
  * If classes of this type exist, it means the ASN.1 specification has unsupported features.
+ * If no classes of this type exists, it means all type assignments were recognized by the compiler.
  * @author Ivan Yourshaw
   */
 public class UnknownType implements Asn1Type {
