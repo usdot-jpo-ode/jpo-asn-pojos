02d2a2bf6277f2277f5af06ca8969eca33ae79d0
mcook42 <mattheworion.cook@gmail.com>
2025-03-26 13:58:29 -0600

    refactor: add @JsonIgnore to isExtensible in Asn1Type

--- jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/types/Asn1Type.java
+++ jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/types/Asn1Type.java
@@ -1,6 +1,8 @@
 package us.dot.its.jpo.asn.runtime.types;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * This is a marker interface.  It is the root of the hierarchy of classes representing
  * ASN.1 types in this package.  Clients can use it to represent any ASN.1 type whose specific
@@ -11,17 +13,19 @@ public interface Asn1Type {
     /**
      * Determines whether this ASN.1 type includes an extension marker.
      *
-     *  <p>For example this is a SEQUENCE type with an extension marker (the three dots):</p>
-     *      * <pre>
+     * <p>For example this is a SEQUENCE type with an extension marker (the three dots):</p>
+     * * <pre>
      *      *   FruitSalad ::= SEQUENCE {
      *      *    fruits       Fruits,
      *      *    servingSize  INTEGER,
      *      *    ...
      *      *    }
      *      * </pre>
-     *      *
+     * *
+     *
      * @return true if the ASN.1 type has an extension marker, false otherwise
      */
+    @JsonIgnore
     default boolean isExtensible() {
         return false;
     }
