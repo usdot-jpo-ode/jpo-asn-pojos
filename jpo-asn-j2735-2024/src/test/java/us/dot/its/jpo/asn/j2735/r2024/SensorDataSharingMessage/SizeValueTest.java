1616234a75c6ca3aef668d9df7b70eefcf55872b
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-03-29 15:10:13 -0600

    RSM, SDSM

---
+++ jpo-asn-j2735-2024/src/test/java/us/dot/its/jpo/asn/j2735/r2024/SensorDataSharingMessage/SizeValueTest.java
@@ -0,0 +1,20 @@
+package us.dot.its.jpo.asn.j2735.r2024.SensorDataSharingMessage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the SizeValue class.
+ */
+public class SizeValueTest {
+  
+  @Test
+  public void constructorTest() {
+    var sizeValue = new SizeValue(1000L);
+    assertThat(sizeValue, notNullValue());
+    assertThat(sizeValue.getValue(), equalTo(1000L));
+  }
+}
