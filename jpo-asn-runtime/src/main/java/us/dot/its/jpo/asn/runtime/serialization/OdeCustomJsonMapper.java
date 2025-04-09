b344a55e912b48bdee359baba33afdbc991ffce1
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-04-09 16:00:33 -0600

    Javadoc

--- jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/serialization/OdeCustomJsonMapper.java
+++ jpo-asn-runtime/src/main/java/us/dot/its/jpo/asn/runtime/serialization/OdeCustomJsonMapper.java
@@ -3,11 +3,40 @@ package us.dot.its.jpo.asn.runtime.serialization;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 
+/**
+ * Configurable ObjectMapper to allow customizations to the JER format produced.
+ *
+ * <p><b>Usage examples</b></p>
+ * <pre>
+ * {@code
+ * // Always cache and reuse ObjectMappers
+ * final static ObjectMapper customMapper = new OdeCustomJsonMapper(true);
+ * final static ObjectMapper standardMapper = new OdeCustomJsonMapper(false); // or = new ObjectMapper();
+ *
+ * // Serialize to json with human readable bitstrings
+ * var humanReadableJson = customMapper.writeValueAsString(bitstring);
+ *
+ * // Deserialize human readable json with human readable bitstrings
+ * ExampleBitstring deserializedFromCustom = customMapper.readValue(humanReadableJson, ExampleBitstring.class);
+ *
+ * // Serialize to standard JER (hex)
+ * var standardJerJson = standardMapper.writeValueAsString(bitstring);
+ *
+ * // Deserialize from standard JER
+ * ExampleBitstring deserializedFromStandard = standardMapper.readValue(standardJerJson, ExampleBitstring.class);
+ *
+ * }
+ * </pre>
+ */
 @Getter
 public class OdeCustomJsonMapper extends ObjectMapper {
 
   private final boolean humanReadableJsonBitstrings;
 
+  /**
+   * @param humanReadableJsonBitstrings Whether to serialize/deserializer BIT STRING values to JSON
+   *                                    using a non-standard human-readable format.
+   */
   public OdeCustomJsonMapper(boolean humanReadableJsonBitstrings) {
     this.humanReadableJsonBitstrings = humanReadableJsonBitstrings;
   }
