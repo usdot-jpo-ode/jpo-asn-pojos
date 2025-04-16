3dd6ac2a8dc7092f20d6c89378643924bc6d8b74
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-04-15 18:50:19 -0600

    Update tests for messages with extensible bitstrings

--- generate-test-data/convert.sh
+++ generate-test-data/convert.sh
@@ -4,5 +4,5 @@ set -x
 
 BASEDIR=./examples/2024/$2
 
-./converter-new-jer -p $1 -iuper -oxer $BASEDIR/$3.bin > $BASEDIR/$3_rt.xml
+#./converter-new-jer -p $1 -iuper -oxer $BASEDIR/$3.uper > $BASEDIR/$3_rt.xml
 ./converter-new-jer -p $1 -iuper -ojer $BASEDIR/$3.bin > $BASEDIR/$3_rt.json
