e780b17504a0490170100605b72a46d29e25d5a4
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-04-22 21:17:21 -0600

    RptRoadwayEventsTests

--- jpo-asn-j2735-2024/src/test/java/us/dot/its/jpo/asn/j2735/r2024/ProbeDataReport/RptRoadwayEventsTest.java
+++ jpo-asn-j2735-2024/src/test/java/us/dot/its/jpo/asn/j2735/r2024/ProbeDataReport/RptRoadwayEventsTest.java
@@ -11,7 +11,6 @@ import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import us.dot.its.jpo.asn.j2735.r2024.BaseSerializeTest;
 
