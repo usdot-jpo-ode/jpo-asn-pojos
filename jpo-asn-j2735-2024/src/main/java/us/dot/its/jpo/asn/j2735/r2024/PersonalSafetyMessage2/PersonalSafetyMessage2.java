9cbfe117cae4853cc033930b8ad2bba37bda889c
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-04-22 21:19:36 -0600

    Regenerate null types with deserializers

--- jpo-asn-j2735-2024/src/main/java/us/dot/its/jpo/asn/j2735/r2024/PersonalSafetyMessage2/PersonalSafetyMessage2.java
+++ jpo-asn-j2735-2024/src/main/java/us/dot/its/jpo/asn/j2735/r2024/PersonalSafetyMessage2/PersonalSafetyMessage2.java
@@ -29,15 +29,15 @@ import us.dot.its.jpo.asn.runtime.types.Asn1Null;
 @JsonDeserialize(using = PersonalSafetyMessage2.PersonalSafetyMessage2Deserializer.class)
 public class PersonalSafetyMessage2 extends Asn1Null {
 
-  public static class PersonalSafetyMessage2Deserializer extends NullDeserializer<PersonalSafetyMessage2> {
-
+  public static class PersonalSafetyMessage2Deserializer
+      extends NullDeserializer<PersonalSafetyMessage2> {
     public PersonalSafetyMessage2Deserializer() {
       super(PersonalSafetyMessage2.class);
     }
 
+    @Override
     protected PersonalSafetyMessage2 construct() {
       return new PersonalSafetyMessage2();
     }
   }
-
 }
