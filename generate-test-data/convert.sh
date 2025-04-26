5bc68c0a320cc3082b994a5ba6bfaaa921be4f49
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-04-26 15:45:11 -0600

    Test generator documentation

--- generate-test-data/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-BASEDIR=./examples/2024/$2
-
-#./converter-new-jer -p $1 -iuper -oxer $BASEDIR/$3.uper > $BASEDIR/$3_rt.xml
-./converter-new-jer -p $1 -iuper -ojer $BASEDIR/$3.bin > $BASEDIR/$3_rt.json
--- generate-test-data/roundtrip.sh
+++ generate-test-data/roundtrip.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
+# Prerequisites:
+# - Linux command line (tested with WSL/Ubuntu on Windows)
+#
+# Script to test "round-tripping" XER and JER messages through the asn1c codec.
+# Accepts an XER and JER file, convets them to UPER, and converts the UPER back
+# to XER and JER.  Outputs UPER, roundtrip XER, JER and diff files.
+#
+# Example usage (prereqs: Java 21+, Git command line):
+#
+# 1. Generate SPAT messages with the Java tool
+#
+#   java -jar testgen-cli.jar -m SPAT -p SPAT -x spat.xml -j spat.json
+#
+# 2. Round trip them through asn1c with this script
+#
+#   ./roundtrip.sh SPAT spat
+#
+
+
 set -x
 
-./converter-new-jer -p $1 -ixer -ouper $2.xml > $2.bin
-./converter-new-jer -p $1 -iuper -oxer $2.bin > ${2}_rt.xml
-./converter-new-jer -p $1 -iuper -ojer $2.bin > ${2}_rt.json
+./converter-new-jer -p $1 -ixer -ouper $2.xml > $2_xml_rt.bin
+./converter-new-jer -p $1 -ijer -ouper $2.json > $2_json_rt.bin
+./converter-new-jer -p $1 -iuper -oxer $2_xml_rt.bin > ${2}_rt.xml
+./converter-new-jer -p $1 -iuper -ojer $2_json_rt.bin > ${2}_rt.json
+diff $2.xml ${2}_rt.xml > ${2}_xml_diff.txt
+diff $2.json ${2}_rt.json > ${2}_json_diff.txt
